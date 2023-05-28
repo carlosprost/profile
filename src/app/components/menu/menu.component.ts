4e63c9bc529c2b94560b942012d5fadcebb77aca
carlosaprost <carlosaprost@gmail.com>
2023-05-28 02:28:31 -0300

    comenzando diseño responsive

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -3,12 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent {
+  abrirMenu(event: Event) {
+    let btn = event.target as HTMLElement;
+    let menuOpt = document.querySelector('.menu-principal') as HTMLElement;
+    if (btn.classList.contains('close_toggle')) {
+      btn.classList.remove('close_toggle');
+      menuOpt.classList.remove('menu-principal-visible');
+    } else {
+      btn.classList.add('close_toggle');
+      menuOpt.classList.add('menu-principal-visible');
+    }
 
-  constructor(){ }
-
-
-  
+    menuOpt.querySelectorAll('a').forEach((link) => {
+      link.addEventListener('click', () => {
+        btn.classList.remove('close_toggle');
+        menuOpt.classList.remove('menu-principal-visible');
+      });
+    });
+  }
 }
